7170aca3b50c92b4925b6eee33291538dd40d7e6
Stefan Luy <sluy1283@gmail.com>
2023-03-20 14:09:46 -0400

    request start

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import "./router.js";
 import "./web/middlewares.js";
 import { server } from "./bootstrap/server.js";
+import log from "./utils/log.js";
 import config from "./config.js";
-
+log("src-index", "start");
 if (config.dev.status) {
   server.listen(config.dev.port, () => {
     console.log("Server started at " + config.dev.port);
