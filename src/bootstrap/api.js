0fbcf4fa663dd61f8fcc3e6c6d2524cdcc7f8321
Stefan Luy <sluy1283@gmail.com>
2023-04-12 16:35:19 -0400

    timeout

--- src/bootstrap/api.js
+++ src/bootstrap/api.js
@@ -2,6 +2,7 @@ const axios = require("axios");
 const config = require("../config");
 const api = axios.create({
   baseURL: config.api.url,
+  timeout: 5000,
   headers: {
     "Content-Type": "application/json",
     api_key: config.api.secret,
